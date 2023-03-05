8a777661962bdfa53f7ee8364053e1a1aa5bb141
hkleine <hendrik-kleine@gmx.de>
2023-03-05 21:35:33 +0100

    routes umbau

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,8 +1,7 @@
-import { stripe } from '../../../utils/stripe';
-import { upsertProductRecord, upsertPriceRecord, manageSubscriptionStatusChange } from '../../../utils/supabase-admin';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest } from 'next/server';
 import Stripe from 'stripe';
-import { Readable } from 'node:stream';
+import { stripe } from '../../../utils/stripe';
+import { manageSubscriptionStatusChange, upsertPriceRecord, upsertProductRecord } from '../../../utils/supabase-admin';
 
 // Stripe requires the raw body to construct the event.
 export const config = {
@@ -11,13 +10,13 @@ export const config = {
   },
 };
 
-async function buffer(readable: Readable) {
-  const chunks = [];
-  for await (const chunk of readable) {
-    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
-  }
-  return Buffer.concat(chunks);
-}
+// async function buffer(readable: ArrayBuffer) {
+//   const chunks = [];
+//   for await (const chunk of readable) {
+//     chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+//   }
+//   return Buffer.concat(chunks);
+// }
 
 const relevantEvents = new Set([
   'product.created',
@@ -30,9 +29,10 @@ const relevantEvents = new Set([
   'customer.subscription.deleted',
 ]);
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const buf = await buffer(req);
-  const sig = req.headers['stripe-signature'];
+export async function POST(req: NextRequest) {
+  const buf = await req.json();
+
+  const sig = req.headers.get('stripe-signature');
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET_LIVE ?? process.env.STRIPE_WEBHOOK_SECRET;
   let event: Stripe.Event;
 
@@ -41,7 +41,9 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
     event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
   } catch (err: any) {
     console.log(`❌ Error message: ${err.message}`);
-    return res.status(400).send(`Webhook Error: ${err.message}`);
+    return new Response(`Webhook Error: ${err.message}`, {
+      status: 400,
+    });
   }
 
   if (relevantEvents.has(event.type)) {
@@ -77,9 +79,11 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
       }
     } catch (error) {
       console.log(error);
-      return res.status(400).send('Webhook error: "Webhook handler failed. View logs."');
+      return new Response('Webhook error: "Webhook handler failed. View logs."', {
+        status: 400,
+      });
     }
   }
 
-  res.json({ received: true });
+  return new Response('', { status: 200 });
 }
