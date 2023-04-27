b23055d12f26a9ce44ca3756f87428efff80ec82
hkleine <hendrik-kleine@gmx.de>
2023-04-27 19:05:00 +0200

    CHECK NEW NEXT VERSION

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
       { loader: '@next/font/google', options: { subsets: ['latin'] } },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.resolve.fallback = {
+            fs: false
+        }
+    }
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
