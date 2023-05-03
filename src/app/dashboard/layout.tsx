c3541c1855b65fead655eea3fa15da0aba5c8115
hkleine <hendrik-kleine@gmx.de>
2023-05-03 10:53:36 +0200

    remove comment

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,7 +6,6 @@ import AuthWrapper from '../components/Auth/AuthWrapper';
 import { RealTimeUserProvider } from '../components/Provider/RealTimeUserProvider';
 
 export const revalidate = 0;
-// export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 
 export default async function DashboardLayout({ children }: { children: ReactNode }) {
