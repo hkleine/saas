d7bb8052f8535f71bcd576e47682b4f623b80329
hkleine <hendrik-kleine@gmx.de>
2023-07-14 20:37:03 +0200

    only new user function

--- supabase/migrations/20230714182148_adapt_new_user_function.sql
+++ supabase/migrations/20230714182148_adapt_new_user_function.sql
@@ -1,9 +1,3 @@
-alter table "public"."earnings" add column "item_id" uuid;
-
-alter table "public"."earnings" add constraint "earnings_item_id_fkey" FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE not valid;
-
-alter table "public"."earnings" validate constraint "earnings_item_id_fkey";
-
 CREATE OR REPLACE FUNCTION public.handle_new_user()
  RETURNS trigger
  LANGUAGE plpgsql
